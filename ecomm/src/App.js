b572dad7aea526ee654ca56d906d6918321505d3
john jorjill <orgil9506@gmail.com>
2020-03-08 17:57:50 +0800

    added shop page

--- ecomm/src/App.js
+++ ecomm/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Switch,Route } from 'react-router-dom';
 
 import './App.css';
 
 import { HomePage } from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
 
 const HatsPage = () => (
   <div>
@@ -14,8 +15,10 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage}/>
-      <Route path='/hats' component={HatsPage}/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
