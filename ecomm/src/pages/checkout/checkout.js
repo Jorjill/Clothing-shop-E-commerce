caffe87b9c8dfe1c676e2023ae86e5cb56741a20
john jorjill <orgil9506@gmail.com>
2020-03-15 20:31:49 +0800

    stripe integration

--- ecomm/src/pages/checkout/checkout.js
+++ ecomm/src/pages/checkout/checkout.js
@@ -6,6 +6,7 @@ import {
   selectCartTotal
 } from '../../redux/cart/cart.selectors';
 import CheckoutItem from '../../components/checkout-item/checkout-item';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button';
 
 import './checkout.scss';
 
@@ -35,6 +36,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     </div>
     {cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem}/>)}
     <div className='total'>TOTAL: ${total}</div>
+      <div className='test-warning'>
+        *Please use the following test credit card for payments*
+        <br/>
+        4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
+      </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
