eaa36df83d3072c23e143316c17090b8d1b8b901
john jorjill <orgil9506@gmail.com>
2020-03-26 19:36:07 +0800

    commit

--- server.js
+++ server.js
@@ -8,19 +8,20 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
-
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
+}
 
 app.listen(port, error => {
   if (error) throw error;
